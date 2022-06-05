7f440059cc768524ecb70668cb60ee0440102a25
Arthur <arthursmf@live.com>
2022-06-05 18:05:05 -0300

    table

---
+++ src/main/resources/static/assets/js/Table.js
@@ -0,0 +1,8 @@
+function mostrarTabela() {
+    document.getElementById('customers').style.display = '';
+}
+
+
+function showTable() {
+    document.getElementById('container-table').style.display = "block";
+}
